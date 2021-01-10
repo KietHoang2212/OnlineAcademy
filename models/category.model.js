6769306accf7671f4645037c74ac5a325c14a0b2
conghuy <conghuynt1999@gmail.com>
2021-01-11 00:25:07 +0700

    Edit category.model.js and course.model.js

--- models/category.model.js
+++ models/category.model.js
@@ -1,18 +1,45 @@
-const db = require('../utils/db');
+const db = require( '../utils/db' );
 
 const TBL_CATEGORIES = 'categories';
 
 module.exports = {
-  all() {
-    return db.load(`select * from ${TBL_CATEGORIES}`);
+  all ()
+  {
+    return db.load( `select * from ${ TBL_CATEGORIES }` );
   },
 
-  allChild(id) {
+  allChild ( id )
+  {
     const sql = `
       select *
       from categories c
-      where c.CatParentID = ${id}
+      where c.CatParentID = ${ id }
     `;
-    return db.load(sql);
+    return db.load( sql );
+  },
+  //huy's
+  add ( entity )
+  {
+    return db.add( entity, TBL_CATEGORIES );
+  },
+  async single ( id )
+  {
+    const rows = await db.load( `select * from ${ TBL_CATEGORIES } where CatID = ${ id }` );
+    if ( rows.length === 0 )
+      return null;
+
+    return rows[ 0 ];
+  },
+  del ( entity )
+  {
+    const condition = { CatID: entity.CatID };
+    return db.del( condition, TBL_CATEGORIES );
+  },
+  patch ( data )
+  {
+    const condition = { CatID: data.CatID },
+      entity = { CatName: data.CatName };
+    return db.patch( data, condition, TBL_CATEGORIES );
   }
+
 };
