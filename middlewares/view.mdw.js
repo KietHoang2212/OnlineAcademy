8f025e1c625ccf0b6882e2ac321ead0fcb380023
conghuy <conghuynt1999@gmail.com>
2021-01-11 00:26:55 +0700

    edit Middleware view.mdw.js adding some helpers

--- middlewares/view.mdw.js
+++ middlewares/view.mdw.js
@@ -1,19 +1,40 @@
-const exphbs = require('express-handlebars');
-const hbs_sections = require('express-handlebars-sections');
-const numeral = require('numeral');
+const exphbs = require( 'express-handlebars' );
+const hbs_sections = require( 'express-handlebars-sections' );
+const numeral = require( 'numeral' );
 
-module.exports = function (app) {
-  app.engine('hbs', exphbs({
+module.exports = function ( app )
+{
+  app.engine( 'hbs', exphbs( {
     defaultLayout: 'main.hbs',
     extname: '.hbs',
     layoutsDir: 'views/_layouts',
     partialsDir: 'views/_partials',
     helpers: {
       section: hbs_sections(),
-      format(val) {
-        return numeral(val).format('0,0');
+      format ( val )
+      {
+        return numeral( val ).format( '0,0' );
+      },
+      add ( a, b )
+      {
+        return a + b;
+      },
+      ifeq: function ( a, b, options )
+      {
+        if ( a == b )
+          return options.fn( this );
+        return options.inverse( this );
+      },
+      ifnoteq: function ( a, b, options )
+      {
+        if ( a == b )
+        {
+
+          return options.inverse( this );
+        }
+        return options.fn( this );
       }
     }
-  }));
-  app.set('view engine', 'hbs');
-}
+  } ) );
+  app.set( 'view engine', 'hbs' );
+};
